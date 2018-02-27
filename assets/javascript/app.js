b6c5a4a07dbdf1ac846bc0b0337a963fb9e35431
stevenhorkey <stevenhorkey@gmail.com>
2018-02-27 11:14:14 -0700

    progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,28 @@
-// api: jocHuSXyJl9YL61rdYLxmy6ccBiHxt0k
+// api-key: jocHuSXyJl9YL61rdYLxmy6ccBiHxt0k
 
-
-
-// var term = $("#gif-input").val();
-var gifTastic ={
+var gifTastic = {
+    // Global array to hold inputed terms. Used to compare later.
     terms : [],
     displayGifs : function(){
         var term = $(this).attr("data-name");
+        // API url which takes the term to search
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + term + "&api_key=jocHuSXyJl9YL61rdYLxmy6ccBiHxt0k&limit=10";
-
+        // AJAX call 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
+            // Logs response in console for developer use.
             console.log(response);
-
+            // Removes non-relevant gifs from previous call
             $('.gif-section').empty();
+            // For each of the ten images in the response, an image element is made, given a relevent image source, given a class of gif-img, and added to the gif section on the html document.
             for (var i = 0; i < response.data.length; i++){
                 var gif = $('<img/>').attr("src", response.data[i].images["original_still"].url);
                 gif.addClass('gif-img');
-                console.log(this);
                 $(".gif-section").append(gif)
             }
+            // When a gif image is clicked on, the ending part of the source url is changed so that the gif stars playing when clicked, and stops when clicked again.
             $('.gif-img').on("click", function(){
                 var src = $(this).attr('src')
                 if(src.includes('_s.gif')){
@@ -34,46 +35,37 @@ var gifTastic ={
         });
     },
     renderButtons : function(){
-        $(".button-selection").empty();
-
+        // Sets term equal to user input
         var term = $("#gif-input").val().trim();  
-
+        // If term hasn't been called before, than a button for it will be made.
         if (!gifTastic.terms.includes(term)){
             if(term !== ""){
-
+                // Pushes term to terms array to check against next time
                 gifTastic.terms.push(term); 
-                // user inputs a term
-                // if that term is not already in terms, than make a button for it.
+                
                 var button = $("<button>");
-
+                // Adds classes to each button
                 button.addClass("gif-button btn btn-standard");
-
+                // Adds relevant data-name and term to each button
                 button.attr("data-name", term);
-
+                // Gives each button the text of the term inputed
                 button.text(term);
-
+                // Dumps each button made into the button section inside the html document
                 $(".button-section").append(button);
-                console.log("render buttons");
             }
         }    
     }
 }
-
+// Assures the document is ready before js is executed.
 $(document).ready(function(){
-
+    // When the gif seach button is clicked, the default reload is dismissed, and the render button function is called.
     $("#find-gif").on("click", function(event) {
         event.preventDefault();
-    
-        var term = $("#gif-input").val().trim();
-    
-        console.log(term);
             
         gifTastic.renderButtons();
     });
-    
+    // When a button is clicked, the corresponding gifs display on the page
     $(document).on("click", ".gif-button", gifTastic.displayGifs);
-    
-    gifTastic.renderButtons();
 })
 
 
